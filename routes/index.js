0eb44bff6c08918fade60a31dadb50902edd41cf
chaleomrat <614259007@webmail.npru.ac.th>
2020-10-05 17:53:01 +0700

    fout

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const myData = require("../data");
 
 router.get("/", (req, res) => {
-  res.render("index", { myData });
+  res.render("index");
 });
 
 module.exports = router;
